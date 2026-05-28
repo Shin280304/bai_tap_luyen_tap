60be464dada824891c8776418e60094837b6c10e
shin280304 <minhnguyen200504@gmail.com>
2026-05-28 20:11:11 +0700

    Initial commit: C# console project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Bai_tap_luyen_tap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("=== Bài tập luyện tập C# ===");
+            Console.WriteLine();
+
+            // Viết code luyện tập của bạn ở đây
+
+
+            Console.WriteLine();
+            Console.WriteLine("Nhấn phím bất kỳ để thoát...");
+            Console.ReadKey();
+        }
+    }
+}
